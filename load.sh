d4c2515fcd83e80069c29084e8d2a18b9da03fdd
Mahesh Balasubramanian <mbalasu2@asu.edu>
2019-03-29 13:01:45 -0700

    Updated load.sh

--- load.sh
+++ load.sh
@@ -1,11 +1,8 @@
+#!/bin/bash
+
 module load eigen3
+module load allineatools
+module load darshan
+module load cray-hdf5-parallel
 module load boost
 module load gsl
-module list
-module unload cray-mpich
-module load pe_archive
-module load cray-mpich/7.4.4
-
-module load cray-hdf5-parallel/1.8.14
-module list
-export LD_LIBRARY_PATH=$HDF5_ROOT/lib:$LD_LIBRARY_PATH
