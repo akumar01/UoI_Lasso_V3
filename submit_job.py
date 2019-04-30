72935cf6cbdcee92e17551974646a7949d25756c
akumar01 <akumar.4db@gmail.com>
2019-04-29 20:01:40 -0700

    ready to test submit_job.py

--- submit_job.py
+++ submit_job.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('data_path')
 parser.add_argument('idx', type=int)
 parser.add_argument('--name', default = 'test')
+parser.add_argument('-r', '--run', action= 'store_true')
 args = parser.parse_args()
 
 # Load the z-scored data
@@ -27,15 +28,30 @@ with open('%s/data%d.h5' % (dirpath, args.idx), 'w') as f:
     f['X'] = X
     f['y'] = y
 
-
 # Re-format the job script to point to the write data file, 
 # and store the results of the fits to the right location
 with open('job', 'r') as f:
     fcontents = f.readlines()
 
-# Edit the where to look for the data 
+# Edit where to look for the data 
 input_line_loc = [i for i in np.arange(len(fcontents)) if 'input=' in fcontents[i]]
-input_line = 'input=%s' % 
+input_line = 'input=%s' % new_data_path
+fcontents[input_line_loc[0]] = input_line
 
-with open('sbatch_%s' % name, 'w') as f:
-    
+
+# Edit the output coef and score files
+coef_output_loc = [i for i in np.arange(len(fcontents) if 'cfile=' in fcontents[i])]
+coef_output = 'cfile=%s'
+fcontents[coef_output_loc[0]] = coef_output
+
+score_output_loc = [i for i in np.arange(len(fcontents) if 'sfile=' in fcontents[i])]
+score_output = 'sfile=%s'
+fcontents[score_output_loc[0]] = score_output
+
+with open('sbatch_%s' % name, 'w') as f:   
+	f.write(fcontents)
+
+if args.run
+    os.system('chmod u+x %s' % run_file)
+    # Submit the job
+    os.system('sbatch %s ' % run_file)
